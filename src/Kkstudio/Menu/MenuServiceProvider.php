e2e2f2b9f1296aeba27e1d17b31b72e797cdb487
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-27 19:45:51 +0100

    first commit

---
+++ src/Kkstudio/Menu/MenuServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Kkstudio\Menu;
+
+use Illuminate\Support\ServiceProvider;
+
+class MenuServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('kkstudio/menu');
+
+		\Route::get('admin/menu', '\Kkstudio\Menu\Controllers\MenuController@admin');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
